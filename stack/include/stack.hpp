1efff459c0cf561b1b13b586531ef8840a2c2855
lnikon <nreplay@list.ru>
2018-09-04 15:28:51 +0400

    stack.hpp content changed

--- stack/include/stack.hpp
+++ stack/include/stack.hpp
@@ -1,14 +1,9 @@
 #pragma once
 
-#include <iostream>
-#include <string>
 #include "singly_linked_list.hpp"
 
-void someFuncFromStack() 
+template <class T>
+class stack 
 {
-    singly_linked_list<std::string> list;
-    list.push_back(std::string("Hello"));
-    list.push_back(std::string(" world"));
-    list.push_back(std::string("!"));
-    list.traverse();
-}
+
+};
